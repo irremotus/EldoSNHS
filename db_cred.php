066e0f06324ee409721ac03a2f6b40bf0b6b3bc5
Kevin Schmittle <kevin@schmittle.net>
2013-12-08 16:29:58 -0700

    do not sync db_cred.php
    credentials are now hidden in unsynced _s_db_cred.php.
    do not sync anything that starts with _s_

--- db_cred.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$host = 'eldosnhs.db.8535028.hostedresource.com';
-	$db_name = 'eldosnhs';
-	$username = 'eldosnhs';
-	$password = 'eaglesD@84';
-?>
--- services/classes/DB.class.php
+++ services/classes/DB.class.php
@@ -4,7 +4,7 @@
 		public static $db = NULL;
 		
 		public static function connect() {
-			include($_SERVER['DOCUMENT_ROOT'].'/db_cred.php');
+			include($_SERVER['DOCUMENT_ROOT'].'/_s_db_cred.php');
 			
 			if(self::$db == NULL) {				
 				self::$db = new PDO("mysql:host={$host};dbname={$db_name};charset=utf8", $username, $password, array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
